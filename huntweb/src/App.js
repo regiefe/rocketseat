313afa53dbac1ce421fb987024a677e6e7ef77b1
Reginaldo F.S <regiefe@gmail.com>
2020-02-21 13:08:08 -0300

    Projeto de estudos da RocketSeat

---
+++ huntweb/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Routes from './routes'
+
+import './styles.css'
+
+import Header from './components/Header'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ huntweb/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from  'react'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header id="main-header">JSHunt</header>
+    )
+    
+}
+
+export default Header
